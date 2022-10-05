2a51f649567006a9db65dbc89ba6afe328ffbeca
pixiedust07 <sharonshaji127001@gmail.com>
2022-10-05 14:27:51 +0530

    Optimize bubble sort, exit if found sorted

--- sorting/bubble_sort.c
+++ sorting/bubble_sort.c
@@ -1,20 +1,27 @@
 #include<stdio.h>
 
 void bubbleSort(int ar[], int size) {
-    int i, j, temp;
+    int i, j, temp, isSwapped;
 
     // loop through the array
     // check for greatest between among adjacent index
     // if an item is greater, swap its position else move on to next two numbers
 
     for ( i = 0; i < size; i++) {
+        isSwapped = 0;
         for ( j = 0; j < size-i; j++) {
             if(ar[j] > ar[j+1]) {
                 // swap its position
                 temp = ar[j];
                 ar[j] = ar[j+1];
                 ar[j+1] = temp;
+                // to determine wherther array has finished sorting or not (if isSwapped = 0 on loop exit, then we can confirmt that the array is already sorted)
+                isSwapped = 1;
             }
+        }
+
+        if(isSwapped == 0) {
+            break;
         }
     }
 
